4526256a418e15910edf36658383498d51047eca
Jacob Calcutt <calcuttj@msu.edu>
2024-08-26 14:52:02 -0500

    Adding necessary lines to wct configure for ups

--- waft/wct-configure-for-ups.sh
+++ waft/wct-configure-for-ups.sh
@@ -56,7 +56,9 @@ env CC=$wct_cc CXX=$wct_cxx FC=wct_fort \
     ./wcb configure \
     --with-tbb=no \
     --with-jsoncpp="$JSONCPP_FQ_DIR" \
-    --with-jsonnet="$JSONNET_FQ_DIR" \
+    --with-jsonnet="$GOJSONNET_FQ_DIR" \
+    --with-spdlog="$SPDLOG_FQ_DIR" \
+    --with-spdlog-lib="$SPDLOG_LIB" \
     --with-eigen-include="$EIGEN_DIR/include/eigen3" \
     --with-root="$ROOT_FQ_DIR" \
     --with-fftw="$FFTW_FQ_DIR" \
@@ -65,5 +67,5 @@ env CC=$wct_cc CXX=$wct_cxx FC=wct_fort \
     --boost-includes="$BOOST_INC" \
     --boost-libs="$BOOST_LIB" \
     --boost-mt \
-    --prefix="$install_dir"
-
+    --prefix="$install_dir" \
+    --build-debug="-O2 -ggdb3 -w" 
